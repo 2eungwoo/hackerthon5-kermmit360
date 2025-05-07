e58ef1fefa968ec7a84043a8ec9bb5def9ec5c87
swlee <swo98@naver.com>
2025-05-07 14:04:33 +0900

    first project setting

---
+++ src/main/java/hackathon/kermmit360/Kermmit360Application.java
@@ -0,0 +1,13 @@
+package hackathon.kermmit360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kermmit360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kermmit360Application.class, args);
+	}
+
+}
