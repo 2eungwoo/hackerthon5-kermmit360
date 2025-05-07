63abe2ec636d279ae652ea0d5870e3d924ce9c74
swlee <swo98@naver.com>
2025-05-07 12:09:16 +0900

    fork 레포지토리 커밋 테스트용 커밋

---
+++ src/test/java/hackathon/kermmit360/Kermmit360ApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.kermmit360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kermmit360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
